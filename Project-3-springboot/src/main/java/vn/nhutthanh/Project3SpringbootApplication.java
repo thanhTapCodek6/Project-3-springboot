2f2a899555ad552990b998a50ae3ec6dfc0c89da
Admin <Admin@192.168.38.1>
2026-09-10 00:38:51 +0700

    Cấu hình sitemesh 3, nhưng đang còn lỗi deploy lên không có nội dung, fix được 1 tí, nhờ AI:

--- Project-3-springboot/src/main/java/vn/nhutthanh/Project3SpringbootApplication.java
+++ Project-3-springboot/src/main/java/vn/nhutthanh/Project3SpringbootApplication.java
@@ -1,14 +1,16 @@
 package vn.nhutthanh;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import vn.nhutthanh.configs.CustomSiteMeshFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 
 @SpringBootApplication
 @Configuration
